cf9ae77ffd4d07f7bb8f87a6c8094a059e08b85f
samkigs <samuel.kihuga@student.moringaschool.com>
2022-06-13 17:44:57 +0300

    fc functions lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (arr) {
+    return [arr[0], arr[1]]
+  }
+  
+  const returnLastTwoDrivers = (arr) => {
+    return arr.slice(-2)
+  }
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+  
+  function createFareMultiplier(fareMulti) {
+    return (fare) => fare * fareMulti
+  }
+  
+  const fareDoubler = createFareMultiplier(2)
+  const fareTripler = createFareMultiplier(3)
+  
+  function selectDifferentDrivers(arr, selectDrivers) {
+    return selectDrivers(arr)}
